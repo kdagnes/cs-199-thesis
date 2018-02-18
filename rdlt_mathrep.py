15b8e86fc8f5695b230f891b56b9821d269d0507
kdagnes <karenmargaret02@gmail.com>
2018-02-18 20:29:59 +0800

    Add files via upload

--- rdlt_mathrep.py
+++ rdlt_mathrep.py
@@ -1,9 +1,9 @@
 #This code is run using Python 3.6.2 and Numpy 1.14.0
+#Added one time checking of L-constraint
 
 import numpy as np
 
 class RDLT:
-
 	def __init__(self, vertices, l_cons, c_cons):
 		self.vertices = []
 		self.l_cons = []
@@ -11,7 +11,7 @@ class RDLT:
 		self.matrix =[]
 		self.arcs = []
 
-#Getting the data
+#Data Pre-processing
 vertices = np.array(input("Vertices:").split())
 arcs = np.array(input("Arcs:").split())
 l_cons=np.array(input("L Constraint:").split())
@@ -24,6 +24,17 @@ for i in range(0, len(arcs)):
  	j = input().split()
  	j = [int(k) for k in j]
  	matrix[i] = j
+
+print(matrix)
+
+X = np.array(input("Input X(Vertex to be traversed):").split())
+X=[int(i) for i in X]
+#L-constraint checking
+l_cons_new = l_cons + matrix.dot(X)
+print("L_k:")
+print(l_cons_new)
+print("Arcs to be traversed:")
+print(l_cons - l_cons_new)
 
 
 
