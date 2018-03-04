6eeb2282f45834e07e7dabea021729e0e3d08649
kdagnes <karenmargaret02@gmail.com>
2018-03-05 02:02:47 +0800

    Add files via upload

--- rdlt_mathrep.py
+++ rdlt_mathrep.py
@@ -30,26 +30,125 @@ def checkValidActivityProfile(activity_profile, vertices, start_vertex, final_ve
 	return 1;
 
 
-def checkLCY (L, C, Y):
+def checkLCY (L, C, Y):	
 
+	LC = [0,0]	
+	if (any(arc < 0 for arc in L)):
+		print ("L not satisfied")
+		LC[0] = 1
+	for i in range(0, len(Y)):
+		if(Y[i] == -1 and (C[i] not in [0, Symbol("eps")])): #No HQ yet
+			print("C not satisfied")
+			LC[1] = 1
+	return LC
+
+
+
+def Generate(S, V, E, start_vertex, final_vertex):
+	C = E['C']
+	L = E['L']
+	arcs = E['ID']
+
+	activity_profile = S
+	P = V
+	if (0 == checkValidActivityProfile(activity_profile, P, start_vertex, final_vertex)):
+		exit("\nInvalid Activity Profile due to inconsistency in the given arcs.\n")
+
+	C_init = C
+	X = {}
+	Z = {}
+	L_vector = []
+	C_vector = []
+
+	L_vector.append(np.array(L))
+	C_vector.append(np.array(C))
+
+	print("Matrix:")
+	matrix = pd.DataFrame(np.zeros(shape=(len(V), len(arcs))), columns=V)
+	matrix = matrix.set_index(arcs)
+
+
+	for arc in arcs:
+		dest_vertex = arc.split("_")
+		dest_vertex = dest_vertex[1]
+		matrix.set_value(arc, dest_vertex, -1)
+	#print (matrix)
+
+	for idx, reach_config in enumerate(S):
+		for i in V:
+			X[i] = 0
+		for j in arcs:
+			Z[j] = 0
+
+		for arc in reach_config:
+			X[arc[1]] = 1
+			Z[arc[0]+'_'+arc[1]] = 1
+
+		Y = matrix.dot([v for v in X.values()])
+
+		L = L + Y*[v for v in Z.values()]
+		C = C - C_init*[v for v in Z.values()]
+		print(L)
+		print(C)
+		print (Y)
+
+		check = checkLCY(L, C, Y)
+		if (any(constraint == 1 for constraint in check)):
+			Err = (idx, check)
+			S = {'L':L_vector, 'C':C_vector}
+			return S, Err
+		L_vector.append(np.array(L))
+		C_vector.append(np.array(C))
+
+	S =  {'L':L_vector, 'C':C_vector}
+	return S, 0
+
+def Verify(S, V, E, start_vertex, final_vertex):
+	S_vector, Err = Generate(S, V, E, start_vertex, final_vertex)
+	if (Err != 0):
+		return 0;
+
+	L = S_vector['L']
+	C = S_vector['C']
+	arcs = E['ID']
+
+	for id_rc, reach_config in enumerate(S):
+		L_result = L[id_rc] - L[id_rc+1]
+		for id_r, r in enumerate(L_result):
+			if (r == 0 and (arcs[id_r].split("_") in reach_config)):
+				return 0
+			if (r == 1 and (arcs[id_r].split("_") not in reach_config)):
+			
+				return 0
+
+		for id_c, c in enumerate(C[id_rc+1]):
+			if(c != 0 and (arcs[id_c].split("_") in reach_config)):
+				print(str(id_rc) + "Fuck" + str(id_c))
+				return 0;
+
+	return S_vector
+
+
+def Sound(S, V, E, start_vertex, final_vertex):
+	S_vector = Verify(S, V, E, start_vertex, final_vertex)
+
+	if (S_vector == 0):
+		return 0
+	else:
+		C = S_vector['C']
+		if (any(c != 0 for c in C[-1])):
+			return 0
+		return 1
+
+#Input RDLT
 
 #Creating the static representation of RDLT
 vertices = np.array(input("Vertices:").split())
-arcs = np.array(input("Arcs:").split())
+arcs= np.array(input("Arcs:").split())
 l_cons=input("L Constraint:").split()
 l_cons=np.array([int(i) for i in l_cons])
 c_cons=input("C Constraint:").split()
 c_cons=np.array([Symbol(i) for i in c_cons])
-
-print("Matrix:")
-matrix = pd.DataFrame(np.zeros(shape=(len(vertices), len(arcs))), columns=vertices)
-matrix = matrix.set_index(arcs)
-
-for arc in arcs:
-	dest_vertex = arc.split("_")
-	dest_vertex = dest_vertex[1]
-	matrix.set_value(arc, dest_vertex, -1)
-#print (matrix)
 
 
 #Activity Profile
@@ -67,33 +166,11 @@ while(1):
 
 	activity_profile.append(rc)
 
-#print(activity_profile)
+	#print(activity_profile)
 
 start_vertex = input("Start vertex:")
 final_vertex = input("Final vertex:")
 
-if (0 == checkValidActivityProfile(activity_profile, vertices, start_vertex, final_vertex)):
-	exit("\nInvalid Activity Profile due to inconsistency in the given arcs.\n")
+E = {'ID':arcs, 'L':l_cons, 'C':c_cons}
 
-L = l_cons
-C = c_cons
-X = {}
-Z = {}
-for reach_config in activity_profile:
-	for i in vertices:
-		X[i] = 0
-	for j in arcs:
-		Z[j] = 0
-
-	for arc in reach_config:
-		X[arc[1]] = 1
-		Z[arc[0]+'_'+arc[1]] = 1
-
-	Y = matrix.dot([v for v in X.values()])
-
-	L = L + Y*[v for v in Z.values()]
-	C = C - c_cons*[v for v in Z.values()]
-	print(L)
-	print(C)
-
-	checkLCY(L, C, Y)
+print(Sound(activity_profile, vertices, E, start_vertex, final_vertex))
