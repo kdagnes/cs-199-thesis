92fd63ce62092cf80273126a7c1b97973284467d
kdagnes <karenmargaret02@gmail.com>
2018-03-05 11:31:48 +0800

    Add files via upload

--- rdlt_mathrep.py
+++ rdlt_mathrep.py
@@ -1,6 +1,8 @@
 #This code is run using Python 3.6.2 and Numpy 1.14.0
 #Added one time checking of L-constraint
 #Added L-checking for whole configuration
+#Removed destination matrix
+
 import numpy as np
 import pandas as pd
 from sympy import *
@@ -10,25 +12,34 @@ def checkValidActivityProfile(activity_profile, vertices, start_vertex, final_ve
 	for i in vertices:
 		source_vertices[i] = 0
 
+	for idx, reach_config in enumerate(activity_profile):
+		for arc in reach_config:
+			if((arc[0] not in vertices) or (arc[1] not in vertices)):
+				print("\nArc "+str(arc[0]) + "_" + str(arc[1]) + " is not valid because " + str(arc[0]) + " or " + str(arc[1]) + " is not in the vertex set." )
+				return 0
+			if(reach_config.count(arc) != 1):
+				print ("\nRepeated arc " +str(arc[0]) + "_" + str(arc[1]) + " at timestep " + str(idx))
+				return 0
+
 	for arc in activity_profile[0]:
 		if(arc[0] == start_vertex):
 			source_vertices[arc[0]] = 1
 		else:
-			print("Not start vertex")
-			return 0;	
+			print("\nNot start vertex")
+			return 0
 
 	for idx, reach_config in enumerate(activity_profile):
 		for arc in reach_config:
 		 	if (source_vertices[arc[0]] == 1):
 		 		source_vertices[arc[1]] = 1
 		 	else:
-		 		print("Not corresponding")
-		 		return 0;
+		 		print("\nNot corresponding")
+		 		return 0
 
 	for arc in activity_profile[-1]:
 		if (arc[1] != final_vertex):
-			print("Not final vertex")
-			return 0;
+			print("\nNot final vertex")
+			return 0
 
 	return 1;
 
@@ -41,7 +52,7 @@ def checkLCY (L, C, Y):
 		LC[0] = 1
 	for i in range(0, len(Y)):
 		if(Y[i] == -1 and (C[i] not in [0, Symbol("eps")])): #No HQ yet
-			print("C not satisfied")
+			print("C not satisfied" + str(C[i]) + " Y[i]:" + str(Y[i]))
 			LC[1] = 1
 	return LC
 
@@ -55,7 +66,11 @@ def Generate(S, V, E, start_vertex, final_vertex):
 	activity_profile = S
 	P = V
 	if (0 == checkValidActivityProfile(activity_profile, P, start_vertex, final_vertex)):
-		exit("\nInvalid Activity Profile due to inconsistency in the given arcs.\n")
+		exit("Invalid Activity Profile.\nThe given activity profile is not verified \nThe given activity profile is not sound")
+		# print("Invalid Activity Profile.\nThe given activity profile is not verified \nThe given activity profile is not sound")
+		# return 0, 0
+	else:
+		print("The given activity profile is valid")
 
 	C_init = C
 	X = {}
@@ -80,9 +95,9 @@ def Generate(S, V, E, start_vertex, final_vertex):
 	# print (matrix)
 
 	for idy, reach_config in enumerate(S):
-		print("\nTimestep: "+str(idy))
-		print(L)
-		print(C)
+		# print("\nTimestep: "+str(idy))
+		# print(L)
+		# print(C)
 		for i in V:
 			X[i] = 0
 		for j in arcs:
@@ -94,22 +109,29 @@ def Generate(S, V, E, start_vertex, final_vertex):
 			X[arc[1]] = 1
 			Z[arc[0]+'_'+arc[1]] = 1
 
+			#print(arc[0]+'_'+arc[1])
 			for y in arcs:
 				lala = y.split("_")
 				if(arc[1] == lala[1]):
 					#print (y)
 					Y[arc[0]+'_'+arc[1]] = -1
+					# print("y:")
+					# print(arc[0]+'_'+arc[1])
 				# else:
 				# 	print("No")
-
+		#print("Z:" + str(len(Z)) + "Y:" + str(len(Y)))
 
 		Y_new = np.array([y for y in Y.values()])
 		Z_new = np.array([v for v in Z.values()])
 		#Y = matrix.dot([v for v in X.values()])
 
 		L = L + Y_new*Z_new
-		C = C - C_init*Z_new
+		
+		for id_c, c_elem in enumerate(C):
+			if (c_elem != 0):
+				C[id_c] = C[id_c] - C_init[id_c]*Z_new[id_c]
 
+		#print (Z_new)
 
 		check = checkLCY(L, C, Y_new)
 		if (any(constraint == 1 for constraint in check)):
@@ -126,7 +148,7 @@ def Generate(S, V, E, start_vertex, final_vertex):
 def Verify(S, V, E, start_vertex, final_vertex):
 	S_vector, Err = Generate(S, V, E, start_vertex, final_vertex)
 	if (Err != 0):
-		print("Error_Verify: Err != 0")
+		print("Error in Generating S'")
 		return 0
 
 	L = S_vector['L']
@@ -156,11 +178,14 @@ def Sound(S, V, E, start_vertex, final_vertex):
 	S_vector = Verify(S, V, E, start_vertex, final_vertex)
 
 	if (S_vector == 0):
-		print("Error_Sound: S_vector = 0")
+		print("The given activity profile is not verified.")
 		return 0
 	else:
+		print ("The given activity profile is verified")
 		C = S_vector['C']
-		if (any(c != 0 for c in C[-1])):
+		if (any(c != 0 for c in C[len(C)-1])):
+			print("Not Sound because of C constraint")
+			print(C[len(C)-1])
 			return 0
 		return 1
 
@@ -195,6 +220,8 @@ while(1):
 start_vertex = input("Start vertex:")
 final_vertex = input("Final vertex:")
 
+print("\n\nResult:")
 E = {'ID':arcs, 'L':l_cons, 'C':c_cons}
 
-print(Sound(activity_profile, vertices, E, start_vertex, final_vertex))
+if(Sound(activity_profile, vertices, E, start_vertex, final_vertex)):
+	print ("The given RDLT is sound")
