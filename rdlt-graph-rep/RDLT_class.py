158c0165f243b713d17e2f5373371fcf7fb355d6
kdagnes <karenmargaret02@gmail.com>
2018-03-09 15:06:42 +0800

    Add files via upload

--- rdlt-graph-rep/RDLT_class.py
+++ rdlt-graph-rep/RDLT_class.py
@@ -1,3 +1,9 @@
+#This code implements the graphical representation of an RDLT
+#This takes in an RDLT input and an acitvity profile with the start and final vertices
+#This checks the validity, verifiability of an activity profile
+#This checks the soundness property of an RDLT given an activity profile
+
+
 class Vertex:
 	def __init__(self, node, node_type, rbs):
 		self.id = node
@@ -50,7 +56,20 @@ class Arc:
 	def lessenLimit(self):
 		self.limit -= 1
 		return
-		
+
+	def possibleTraversal(self):
+		if(0 not in self.time):
+			return 0
+		else:
+			return 1
+
+	def checkIfTraversed(self):
+		if (self.time[0] != 0):
+			return 1
+		return 0
+
+	def updateTime(self, time_traversed):
+		self.time[self.time.index(0)] = time_traversed
 	
 
 class RDLT:
@@ -71,7 +90,7 @@ class RDLT:
 	def addArc(self, edge, fromNode, toNode, constraint, limit):
 		if(edge in self.arcs.keys()):
 			return 0
-		if(not(self.vertices[fromNode] and self.vertices[toNode])):
+		if((fromNode not in self.vertices.keys()) or (toNode not in self.vertices.keys())):
 			return 0
 
 		from_vertex = self.vertices[fromNode]
@@ -102,19 +121,17 @@ def validifyActivityProfile (rdlt, activity_profile, start_vertex, final_vertex)
 
 	#check if each arc is a valid traversal for each config
 	for id_config, config in enumerate(activity_profile):
-		print ("Reachability Configuration: " + str(id_config))
+		print ("Reachability Configuration: " + str(id_config+1))
 		y_vertices = []
 
 		#check if L-constraint is satisfied
 		#and get the goal vertices
 		for arc in config:
-			arc_id = arc[0] + "_" + arc[1]
-			print(rdlt.arcs[arc_id].getLimit())
-			if(rdlt.arcs[arc_id].getLimit() <= 0):
+			if(not(rdlt.arcs[arc[0] + "_" + arc[1]].possibleTraversal())):
 				print ("Limit traversal is reached")
 				return 0
-			else:
-				rdlt.arcs[arc_id].lessenLimit()
+
+			rdlt.arcs[arc[0] + "_" + arc[1]].updateTime(id_config+1)
 
 			if(arc[1] not in y_vertices):
 				y_vertices.append(arc[1])
@@ -125,13 +142,12 @@ def validifyActivityProfile (rdlt, activity_profile, start_vertex, final_vertex)
 				if (arc[1] == goal_vertex):
 					arc_list.append(arc[0]+"_"+arc[1])
 				
-			print (goal_vertex)
 			if (checkUnconstrained(rdlt, goal_vertex, arc_list) == 0):
-				print ("Not unconstrained.")
+				print ("Traversal to " +goal_vertex+" is NOT UNCONSTRAINED.")
 				return 0
-			else:
-				print ("Unconstrained")
-			
+			else:	
+				print ("Traversal to " +goal_vertex+" is UNCONSTRAINED.")
+
 	return 1
 
 
@@ -139,8 +155,12 @@ def checkUnconstrained(rdlt, goal_vertex, arc_list):
 	#preprocessing
 	vy_constraint = []
 	xy_constraint = []
+	checkIfTraversed = 1 #this variable keeps track if all the arcs have been traversed. essential for condition #3 of unconstrained arc
 	for arc in rdlt.vertices[goal_vertex].getInArc():
 		cons = arc.getConstraint()
+		if (not(arc.checkIfTraversed())):
+			checkIfTraversed = 0
+
 		if(cons not in vy_constraint):
 			vy_constraint.append(cons)
 
@@ -152,12 +172,15 @@ def checkUnconstrained(rdlt, goal_vertex, arc_list):
 	if ("EPSILON" not in xy_constraint):
 			xy_constraint.append("EPSILON")
 
-	print (xy_constraint)
-	print (vy_constraint)
+	# print (xy_constraint)
+	# print (vy_constraint)
+	# print (arc_list)
 	#actualchecking
 	for constraint in vy_constraint:
 		if (constraint not in xy_constraint):
-			return 0
+			#check for condition 3
+			if(all(constraint == "EPSILON" for constraint in xy_constraint) and (checkIfTraversed == 0)):
+				return 0
 	return 1
 	
 
@@ -173,28 +196,61 @@ def createRDLT():
 	while(e!=0):
 		e_id, e_cons, e_limit = input().split()
 		arc = e_id.split("_")
-		rdlt.addArc(e_id, arc[0], arc[1], e_cons, int(e_limit))
+
+		success = rdlt.addArc(e_id, arc[0], arc[1], e_cons, int(e_limit))
+		if (success == 0):
+			print ("Unsuccessful creation of arc")
+			return 0
 		e -= 1
 
 	return rdlt
 
-def getActivityProfile():
+def getActivityProfile(start_vertex, final_vertex, vertices, arcs):
 	total_config = input()
 	total_config = int(total_config)
 	activity_profile = []
-	while(total_config != 0):
+
+	source_vertices = {}
+	for i in vertices:
+		source_vertices[i] = 0
+	source_vertices[start_vertex] = 1
+	
+	for i in range(0, total_config):
 		config = input().split()
 		reach_config = []
 		for conf in config:
-			con = conf.split("_")
-			if (con in reach_config):
-				print("Repeated arc: " + conf )
+			if (conf not in arcs):
+				print ("Given arc is not in the arc set.")
 				return 0
+			con = conf.split("_")
+			if (i == 0 and con[0] != start_vertex):
+				print ("Invalid start vertex." + conf)
+				return 0
+			if (i == total_config and con[1] != final_vertex):
+				print ("Invalid final vertex." + conf + str(i))
+				return 0
+			if ([con[0], con[1]] in reach_config):
+				print ("Repeated arc " + conf + " at reachable configuration:"+ str(i))
+				return 0
+
+			if (source_vertices[con[0]] != 1):
+				print("Not valid arc.")
+				return 0
+
+			source_vertices[con[1]] = 1
 			reach_config.append([con[0], con[1]])
+
 		activity_profile.append(reach_config)
 
-		total_config -= 1
+	total_config -= 1
 	return activity_profile
+
+def checkSound(rdlt):
+	for arc in rdlt.arcs.values():
+		if (arc.checkIfTraversed() == 0):
+			return 0
+	return 1
+
 
 #Creating the RDLT and activity profile
 rdlt = createRDLT()
@@ -202,19 +258,28 @@ if (rdlt == 0):
 	print("Unsuccessful creation of RDLT.")
 	exit()
 
-activity_profile = getActivityProfile()
-if(activity_profile == 0):
-	print ("Unsuccessful creation of activity profile.")
-	exit()
 start_vertex, final_vertex = input().split()
 if(not(start_vertex and final_vertex)):
 	print ("Unsuccessful identification of start and final vertex")
 	exit()
-valid = validifyActivityProfile(rdlt, activity_profile, start_vertex, final_vertex)
-if(valid == 1):
-	print("Activity Profile is VALID.")
+
+activity_profile = getActivityProfile(start_vertex, final_vertex, list(rdlt.vertices.keys()), list(rdlt.arcs.keys()))
+if(activity_profile == 0):
+	print ("Activity profile is NOT VALID.")
 	exit()
 else:
-	print("Activity profile is NOT VALID.")
+	print ("Activity profile is VALID.")
+
+valid = validifyActivityProfile(rdlt, activity_profile, start_vertex, final_vertex)
+if(valid == 1):
+	print("Activity Profile is VERIFIED.")
+else:
+	print("Activity profile is NOT VERIFIED.")
+
+sound = checkSound(rdlt)
+if (sound):
+	print ("RDLT is SOUND")
+else:
+	print ("RDLT is NOT SOUND")
 
 #consider double entry of arcs and not existent vertices
